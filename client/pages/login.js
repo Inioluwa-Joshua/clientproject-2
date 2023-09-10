3b0cfc6044a009761d7fcf8d32405f91656dd438
Inioluwa Joshua <joshuainioluwa15@gmail.com>
2023-09-10 15:43:49 +0100

    fix

--- client/pages/login.js
+++ client/pages/login.js
@@ -3,19 +3,19 @@ import Navbar from "../components/navbar";
 // import { Container } from "../components/globalComp/wrapper";
 
 const login = () => {
-  const Container = styled.div`
-    width: 100vw;
-    height: 100vh;
-    scroll-snap-type: y mandatory;
-    scroll-behavior: smooth;
-    overflow-y: auto;
-    scrollbar-width: none;
-    color: white;
-    background: url("./img/bg.jpeg");
-    &::-webkit-scrollbar {
-      display: none;
-    }
-  `;
+  // const Container = styled.div`
+  //   width: 100vw;
+  //   height: 100vh;
+  //   scroll-snap-type: y mandatory;
+  //   scroll-behavior: smooth;
+  //   overflow-y: auto;
+  //   scrollbar-width: none;
+  //   color: white;
+  //   background: url("./img/bg.jpeg");
+  //   &::-webkit-scrollbar {
+  //     display: none;
+  //   }
+  // `;
   return (
     <>
       <div className="px-[0.8rem] lg:px-[3rem]">
